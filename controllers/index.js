2bc4af77cb0399fb3a2609fc461899ea242821f6
SiphoChris <siphonkebe@gmail.com>
2024-08-08 10:41:18 +0200

    done with the products crud operations

--- controllers/index.js
+++ controllers/index.js
@@ -1,103 +1,201 @@
-import path from 'node:path'
+import path from "node:path";
 import { connection as db } from "../config/index.js";
 
 function getUsers(_req, res) {
-    try {
-      const strQry = `
+  try {
+    const strQry = `
                 SELECT *
                 from Users;
                 `;
-      db.query(strQry, (err, results) => {
-        if (err) throw new Error(`Unable to fetch all users`);
-        res.json({
-          status: res.statusCode,
-          results: results,
-        });
-      });
-    } catch (e) {
+    db.query(strQry, (err, results) => {
+      if (err) throw new Error(`Unable to fetch all users`);
       res.json({
-        status: 404,
-        msg: e.message,
+        status: res.statusCode,
+        results: results,
       });
-    }
+    });
+  } catch (e) {
+    res.json({
+      status: 404,
+      msg: e.message,
+    });
   }
-
-  function getHomePage(_req, res){
-    res.status(200).sendFile(path.resolve("./static/html/index.html"));
 }
 
-function getUser(req, res){
-    try {
-      let userID = +req.params.userID
-      const strQry = `
+function getUser(req, res) {
+  try {
+    let userID = +req.params.userID;
+    const strQry = `
                 SELECT *
                 from Users
                 where userID = ${userID};
                 `;
-      db.query(strQry, (err, results) => {
-        if (err) throw new Error(`Unable to fetch all users`);
-        res.json({
-          status: res.statusCode,
-          results: results[0],
-        });
-      });
-    } catch (e) {
-      res.json({
-        status: 404,
-        msg: e.message,
-      });
-    }
-  }
-
-  function registerUser(req, res) {
-    let {userID, userName, userSurname, userAge, userEmail, userPwd} = req.body;
-    let strQry = `INSERT INTO Users (userID, userName, userSurname, userAge, userEmail, userPwd)
-                VALUES (?, ?, ?, ?, ?, ?)`;
-    db.query(strQry, [userID, userName, userSurname, userAge, userEmail, userPwd], (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: 'Unable to register user' });
-        }
-        res.json({
-            status: res.statusCode,
-            results: results,
-        });
-    });
-};
-
-function updateUser(req, res) {
-    let {userID, userName, userSurname, userAge, userEmail, userPwd} = req.body;
-    const userToBeUpdated = req.params.userID;
-    let strQry = `UPDATE Users
-                SET userName = ?, userSurname = ?, userAge = ?, userEmail = ?, userPwd = ?
-                WHERE userID = ${userToBeUpdated};`;
-    db.query(strQry, [userName, userSurname, userAge, userEmail, userPwd, userID], (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: 'Unable to update user' });
-        }
-        res.json({
-            status: res.statusCode,
-            results: results,
-        });
-    });
-};
-
-function deleteUser(req, res) {
-    const userToBeDeleted = req.params.userID;
-    let strQry = `DELETE FROM Users
-                WHERE userID = ${userToBeDeleted};`;
     db.query(strQry, (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: 'Unable to delete user' });
-        }
-        res.json({
-            status: res.statusCode,
-            results: results,
-        });
+      if (err) throw new Error(`Unable to fetch all users`);
+      res.json({
+        status: res.statusCode,
+        results: results[0],
+      });
     });
-};
-
-  function getErrorPage(_req, res) {
-    res.sendFile(path.resolve("./static/html/error.html"));
+  } catch (e) {
+    res.json({
+      status: 404,
+      msg: e.message,
+    });
+  }
 }
 
-  export {getUsers, getHomePage, getUser, getErrorPage, registerUser, updateUser, deleteUser}
+function registerUser(req, res) {
+  let { userID, userName, userSurname, userAge, userEmail, userPwd } = req.body;
+  let strQry = `INSERT INTO Users (userID, userName, userSurname, userAge, userEmail, userPwd)
+                VALUES (?, ?, ?, ?, ?, ?)`;
+  db.query(
+    strQry,
+    [userID, userName, userSurname, userAge, userEmail, userPwd],
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: "Unable to register user" });
+      }
+      res.json({
+        status: res.statusCode,
+        results: results,
+      });
+    }
+  );
+}
+
+function updateUser(req, res) {
+  let { userID, userName, userSurname, userAge, userEmail, userPwd } = req.body;
+  const userToBeUpdated = req.params.userID;
+  let strQry = `UPDATE Users
+                SET userName = ?, userSurname = ?, userAge = ?, userEmail = ?, userPwd = ?
+                WHERE userID = ${userToBeUpdated};`;
+  db.query(
+    strQry,
+    [userName, userSurname, userAge, userEmail, userPwd, userID],
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: "Unable to update user" });
+      }
+      res.json({
+        status: res.statusCode,
+        results: results,
+      });
+    }
+  );
+}
+
+function deleteUser(req, res) {
+  const userToBeDeleted = req.params.userID;
+  let strQry = `DELETE FROM Users
+                WHERE userID = ${userToBeDeleted};`;
+  db.query(strQry, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "Unable to delete user" });
+    }
+    res.json({
+      status: res.statusCode,
+      results: results,
+    });
+  });
+}
+
+function getProducts(_req, res) {
+  res.sendFile(path.resolve("./static/html/products.html"));
+}
+
+function getProduct(req, res) {
+  try {
+    let productID = +req.params.userID;
+    const strQry = `
+                  SELECT *
+                  from Products
+                  where prodID = ${userID};
+                  `;
+    db.query(strQry, (err, results) => {
+      if (err) throw new Error(`Unable to fetch all users`);
+      res.json({
+        status: res.statusCode,
+        results: results[0],
+      });
+    });
+  } catch (e) {
+    res.json({
+      status: 404,
+      msg: e.message,
+    });
+  }
+}
+
+function getErrorPage(_req, res) {
+  res.sendFile(path.resolve("./static/html/error.html"));
+}
+
+function addProduct(req, res) {
+  let { prodID, prodName, prodQuantity, prodPrice, prodURL, userID } = req.body;
+  let strQry = `INSERT INTO Products (prodID, prodName, prodQuantity, prodPrice, prodURL, userID)
+                  VALUES (?, ?, ?, ?, ?, ?)`;
+  db.query(
+    strQry,
+    [prodID, prodName, prodQuantity, prodPrice, prodURL, userID],
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: "Unable to create product" });
+      }
+      res.json({
+        status: res.statusCode,
+        results: results,
+      });
+    }
+  );
+}
+
+function updateProduct(req, res) {
+    let {  prodID, prodName, prodQuantity, prodPrice, prodURL, userID } = req.body;
+    const productToBeUpdated = req.params.prodID;
+    let strQry = `UPDATE Products
+                  SET prodID = ?, prodName = ?, prodQuantity = ?, prodPrice = ?, prodURL = ?, userID = ?
+                  WHERE prodID = ${productToBeUpdated};`;
+    db.query(
+      strQry,
+      [prodID, prodName, prodQuantity, prodPrice, prodURL, userID],
+      (err, results) => {
+        if (err) {
+          return res.status(500).json({ error: "Unable to update product" });
+        }
+        res.json({
+          status: res.statusCode,
+          results: results,
+        });
+      }
+    );
+  }
+
+function deleteProduct(req, res) {
+const productToBeDeleted = req.params.prodID;
+let strQry = `DELETE FROM Products
+                WHERE prodID = ${productToBeDeleted};`;
+db.query(strQry, (err, results) => {
+    if (err) {
+    return res.status(500).json({ error: "Unable to delete product" });
+    }
+    res.json({
+    status: res.statusCode,
+    results: results,
+    });
+});
+}
+
+export {
+  getUsers,
+  getUser,
+  getErrorPage,
+  registerUser,
+  updateUser,
+  deleteUser,
+  getProducts,
+  getProduct,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,24 @@
 import express from "express";
 import {
   getUsers,
-  getHomePage,
   getUser,
   getErrorPage,
   registerUser,
   updateUser,
   deleteUser,
+  getProducts,
+  getProduct,
+  addProduct,
+  updateProduct,
+  deleteProduct,
 } from "../controllers/index.js";
 import bodyParser from "body-parser";
 
 const server = express();
 const router = express.Router();
 
-// home page end-point : gets home page
-router.get("^/$|/eShop", getHomePage);
+// middleware
+router.use(bodyParser.json());
 
 // users end-point : gets all users
 router.get("/users", getUsers);
@@ -23,13 +27,30 @@ router.get("/users", getUsers);
 router.get("/user/:userID", getUser);
 
 // register end-point : registers a user
-router.post("/register", bodyParser.json(), registerUser);
+router.post("/register", registerUser);
 
 // update end-point : updates a user
-router.patch("/users/update/:userID", bodyParser.json(), updateUser);
+router.patch("/users/update/:userID", updateUser);
 
+// delete end-point : deletes a user
 router.delete("/users/delete/:userID", deleteUser);
 
+// products end-points : get all products
+router.get("^/$|/products", getProducts);
+
+// product end-point : gets a product by id
+router.get("/products/:userID", getProduct);
+
+// product end-point : adds a product
+router.post("/products/add", addProduct);
+
+// product end-point : updates a product
+router.patch("/products/update/:prodID", updateProduct);
+
+// product end-point : deletes a product
+router.delete("/products/delete/:prodID", deleteProduct);
+
+// error end-point : returns error page
 router.get("*", getErrorPage);
 
 export { router };
